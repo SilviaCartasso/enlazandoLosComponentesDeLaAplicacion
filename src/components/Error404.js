8d3202d12f6ffe60545b92e16c7192c86e23011f
SilviaCartasso <smc.03673@gmail.com>
2023-06-25 23:09:55 -0300

    se sube practica integradora

---
+++ src/components/Error404.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Error404 = () => {
+    return (
+        <div className=" text-danger" >
+            <h1 className="text-center">Disculpe, la página no esta disponible</h1>
+        </div>
+    )
+}
+export default Error404;
